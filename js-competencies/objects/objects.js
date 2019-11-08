ba026fa4cd55d2f831aad66ae6fae23ee22404ce
grayluker05 <graysonluker@gmail.com>
2019-11-08 13:58:54 -0700

    comeback for later

--- js-competencies/objects/objects.js
+++ js-competencies/objects/objects.js
@@ -12,13 +12,13 @@ var duck = {
 //1. Using dot notation to access the duck's "name" property.
 // var accessByDotNotation = duck."name"
 // var accessByDotNotation = Object[duck].name
-// var accessByDotNotation = duck.name
+var accessByDotNotation = duck.name
 
 //2. Using bracket notation to access duck's "bites" property.
 // var accessByBracketNotation = duck{"bites"}
 // var accessByBracketNotation = Object[duck][bites]
 // var accessByBracketNotation = duck[bites]
-// var accessByBracketNotation = duck["bites"]
+var accessByBracketNotation = duck["bites"]
 // var accessByBracketNotation = duck(bites)
 
 
@@ -34,7 +34,9 @@ var person = {
   job: "Leader of Ghost Tours"
 }
 
-//CODE HERE
+person.moveCities = function(newCity){
+  person.city = newCity
+}
 
 
 ///////////////////Problem 3///////////////////
